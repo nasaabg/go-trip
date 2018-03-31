966ac3d9758660bb272565ceb0575ad60a6ec75f
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2018-03-31 16:07:42 +0200

    Add create mutation for Trips

---
+++ app/graphql/resolvers/create_trip.rb
@@ -0,0 +1,13 @@
+class Resolvers::CreateTrip < GraphQL::Function
+  argument :name, !types.String
+  argument :description, !types.String
+
+  type Types::TripType
+
+  def call(_obj, args, _ctx)
+    Trip.create!(
+      name: args[:name],
+      description: args[:description],
+    )
+  end
+end
